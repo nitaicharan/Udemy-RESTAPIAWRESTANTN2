3097b3f99362689e7b13ed5ce105b7dda7770a6d
Nitai Charan <nitaicharan@gmail.com>
2020-11-02 20:43:12 -0300

    66. Organizing Framework Constants

---
+++ src/test/java/com/nitaicharan/udemy_restapiawrestantn2/constants/Auth.java
@@ -0,0 +1,10 @@
+package com.nitaicharan.udemy_restapiawrestantn2.constants;
+
+public class Auth {
+    private String TWITTER_KEY = System.getenv("TWITTER_KEY");
+    private String TWITTER_SECRET_KEY = System.getenv("TWITTER_SECRET_KEY");
+    private String TWITTER_TOKEN = System.getenv("TWITTER_TOKEN");
+    private String TWITTER_SECRET_TOKEN = System.getenv("TWITTER_SECRET_TOKEN");
+    private String TWITTER_USER_ID = System.getenv("TWITTER_USER_ID");
+    private String TWITTER_USER_NAME = System.getenv("TWITTER_USER_NAME");
+}
---
+++ src/test/java/com/nitaicharan/udemy_restapiawrestantn2/constants/Path.java
@@ -0,0 +1,6 @@
+package com.nitaicharan.udemy_restapiawrestantn2.constants;
+
+public class Path {
+    public static final String BASE_URL = "https://api.twitter.com";
+    public static final String STATUSES = "/1.1/statuses";
+}
