ec57730d9bbc3b5fa229e9fffe6b8ef74c1c97e6
Nitai Charan <nitaicharan@gmail.com>
2020-11-02 20:57:52 -0300

    67. Project Dependencies Setup

--- src/test/java/com/nitaicharan/udemy_restapiawrestantn2/constants/Auth.java
+++ src/test/java/com/nitaicharan/udemy_restapiawrestantn2/constants/Auth.java
@@ -1,10 +1,10 @@
 package com.nitaicharan.udemy_restapiawrestantn2.constants;
 
 public class Auth {
-    private String TWITTER_KEY = System.getenv("TWITTER_KEY");
-    private String TWITTER_SECRET_KEY = System.getenv("TWITTER_SECRET_KEY");
-    private String TWITTER_TOKEN = System.getenv("TWITTER_TOKEN");
-    private String TWITTER_SECRET_TOKEN = System.getenv("TWITTER_SECRET_TOKEN");
-    private String TWITTER_USER_ID = System.getenv("TWITTER_USER_ID");
-    private String TWITTER_USER_NAME = System.getenv("TWITTER_USER_NAME");
+    public static final String TWITTER_KEY = System.getenv("TWITTER_KEY");
+    public static final String TWITTER_SECRET_KEY = System.getenv("TWITTER_SECRET_KEY");
+    public static final String TWITTER_TOKEN = System.getenv("TWITTER_TOKEN");
+    public static final String TWITTER_SECRET_TOKEN = System.getenv("TWITTER_SECRET_TOKEN");
+    public static final String TWITTER_USER_ID = System.getenv("TWITTER_USER_ID");
+    public static final String TWITTER_USER_NAME = System.getenv("TWITTER_USER_NAME");
 }
